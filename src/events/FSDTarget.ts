fceb3b2bfeee685e14627aa479c5a5d3d53e46ec
Martyn <sethiroth66@gmail.com>
2020-06-24 17:11:01 +0100

    Removing terrible attempt at TypeScript. ¯\_(ツ)_/¯

--- src/events/FSDTarget.ts
+++
@@ -1,9 +0,0 @@
-import {EliteJournal} from "./_EliteJournal";
-
-export interface FSDTarget extends EliteJournal {
-  timestamp: string
-  event: "FSDTarget"
-  Name: string,
-  SystemAddress: bigint,
-  RemainingJumpsInRoute: number
-}
